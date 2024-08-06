51fa577704e5283c78888ac80d0154dc0f69c301
ddubbu <hello.ddubbu@gmail.com>
2024-08-06 23:16:46 +0900

    chore: prettier 전체 적용 #21

--- apps/concern.py
+++ apps/concern.py
@@ -8,69 +8,74 @@ load_dotenv()
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 
-concern_bp = Blueprint('concern_bp', __name__)
+concern_bp = Blueprint("concern_bp", __name__)
 
 client = MongoClient(MONGO_DB_URI)
 db = client[MONGO_DB_NAME]
 
+
 ## HTML을 주는 부분
-@concern_bp.route('/')
+@concern_bp.route("/")
 def home():
-   return render_template('index.html')
+    return render_template("index.html")
+
 
 ## todo 전체조회
-@concern_bp.route('/todo', methods=['GET'])
+@concern_bp.route("/todo", methods=["GET"])
 def todo():
     result = list(db.todos.find({}))
-   
-    for i in result:
-        i['_id'] = str(i['_id'])
 
-    if len(result) > 0 :
-        return jsonify({'result':'success', 'lists':result, 'msg':'GET 성공!'})
-    else :
-        return jsonify({'result':'fail', 'msg':'결과 없음'})
+    for i in result:
+        i["_id"] = str(i["_id"])
+
+    if len(result) > 0:
+        return jsonify({"result": "success", "lists": result, "msg": "GET 성공!"})
+    else:
+        return jsonify({"result": "fail", "msg": "결과 없음"})
+
 
 ## todo 입력
-@concern_bp.route('/todo', methods=['POST'])
+@concern_bp.route("/todo", methods=["POST"])
 def save():
-    text_request = request.form['text_request']
-    status_request = request.form['status_request']
-    todo = {
-        'text': text_request,
-        'status': status_request
-    }
+    text_request = request.form["text_request"]
+    status_request = request.form["status_request"]
+    todo = {"text": text_request, "status": status_request}
 
     db.todos.insert_one(todo)
-    return jsonify({'result': 'success', 'msg':'POST 성공!'})
+    return jsonify({"result": "success", "msg": "POST 성공!"})
+
 
 ## 상태 업데이트
-@concern_bp.route('/todo/updateStatus', methods=['POST'])
+@concern_bp.route("/todo/updateStatus", methods=["POST"])
 def updateStatus():
-    id_request = request.form['id_give']
-    status_request = request.form['status_give']
-    msg_response = ''
-    if status_request == 'done':
-        msg_response = '할 일 체크 완료!'
-    else :
-        msg_response = '할 일 완료 취소!'
-    db.todos.update_one({"_id" : ObjectId(id_request)}, {'$set' : {"status" : status_request}})
+    id_request = request.form["id_give"]
+    status_request = request.form["status_give"]
+    msg_response = ""
+    if status_request == "done":
+        msg_response = "할 일 체크 완료!"
+    else:
+        msg_response = "할 일 완료 취소!"
+    db.todos.update_one(
+        {"_id": ObjectId(id_request)}, {"$set": {"status": status_request}}
+    )
 
-    return jsonify({'result': 'success', 'msg': msg_response})
+    return jsonify({"result": "success", "msg": msg_response})
+
 
 ## text 업데이트
-@concern_bp.route('/todo/updateText', methods=['POST'])
+@concern_bp.route("/todo/updateText", methods=["POST"])
 def updateText():
-    id_request = request.form['id_give']
-    text_request = request.form['text_give']
+    id_request = request.form["id_give"]
+    text_request = request.form["text_give"]
 
-    db.todos.update_one({"_id" : ObjectId(id_request)}, {'$set' : {"text" : text_request}})
+    db.todos.update_one({"_id": ObjectId(id_request)}, {"$set": {"text": text_request}})
 
-    return jsonify({'result': 'success', 'msg': '할 일 업데이트 완료!'})
+    return jsonify({"result": "success", "msg": "할 일 업데이트 완료!"})
+
 
 ## todo 삭제
-@concern_bp.route('/todo/delete', methods=['POST'])
+@concern_bp.route("/todo/delete", methods=["POST"])
 def delete():
-    id_request = request.form['id_give']
-    db.todos.delete_one({"_id" : ObjectId(id_request)})
-    return jsonify({'result': 'success', 'msg': '할 일 삭제 완료!'})
+    id_request = request.form["id_give"]
+    db.todos.delete_one({"_id": ObjectId(id_request)})
+    return jsonify({"result": "success", "msg": "할 일 삭제 완료!"})
